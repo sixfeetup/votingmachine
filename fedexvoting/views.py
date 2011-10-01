50428baf0a751924b7eb439606dc1269eb943ba2
Clayton Parker <clayton@sixfeetup.com>
2011-10-01 18:56:06 -0400

    remove boilerplate model

--- fedexvoting/views.py
+++ fedexvoting/views.py
@@ -1,6 +1,2 @@
 from pyramid.view import view_config
-from fedexvoting.models import MyModel
 
-@view_config(context=MyModel, renderer='fedexvoting:templates/mytemplate.pt')
-def my_view(request):
-    return {'project':'fedexvoting'}
