1d4b93cc13d8b72958976ff1e371bcee67c12880
Clayton Parker <clayton@sixfeetup.com>
2011-10-01 18:58:09 -0400

    Add the root object

--- fedexvoting/models.py
+++ fedexvoting/models.py
@@ -1,9 +1,11 @@
 from persistent.mapping import PersistentMapping
 
+class PollingPlace(PersistentMapping):
     __parent__ = __name__ = None
 
 def appmaker(zodb_root):
     if not 'app_root' in zodb_root:
+        app_root = PollingPlace()
         zodb_root['app_root'] = app_root
         import transaction
         transaction.commit()
--- fedexvoting/views.py
+++ fedexvoting/views.py
@@ -1,2 +1,8 @@
 from pyramid.view import view_config
+from fedexvoting.models import PollingPlace
 
+
+@view_config(context=PollingPlace,
+    renderer='fedexvoting:templates/polling_place.pt')
+def polling_view(request):
+    return {}
