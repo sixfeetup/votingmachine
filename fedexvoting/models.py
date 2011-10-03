6c04952f0f5ebf8f0df056fffdbfab25e12c9f1b
Clayton Parker <clayton@sixfeetup.com>
2011-10-03 00:56:54 -0400

    PEP8

--- fedexvoting/models.py
+++ fedexvoting/models.py
@@ -68,6 +68,8 @@ class Team(Persistent):
         """A FedEx day team"""
         self.title = title
         self.description = description
+
+
 def appmaker(zodb_root):
     if not 'app_root' in zodb_root:
         app_root = PollingPlace()
