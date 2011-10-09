1323156f6f768779194590a4c5082ce396f3ee92
Clayton Parker <clayton@sixfeetup.com>
2011-10-08 21:42:10 -0400

    add some comments

--- fedexvoting/__init__.py
+++ fedexvoting/__init__.py
@@ -18,12 +18,16 @@ def main(global_config, **settings):
     fedexvoting_templates = resource_filename('fedexvoting', 'templates')
     search_path = (fedexvoting_templates, deform_templates)
     Form.set_zpt_renderer(search_path)
+    # pyramid configuration
     config = Configurator(root_factory=root_factory, settings=settings)
+    # subscriber for base template setup
     config.add_subscriber(
         'fedexvoting.subscribers.add_base_template',
         'pyramid.events.BeforeRender')
+    # add static views for this app and deform
     config.add_static_view('static', 'fedexvoting:static', cache_max_age=3600)
     config.add_static_view(
         'deform_static', 'deform:static', cache_max_age=3600)
+    # find views
     config.scan('fedexvoting')
     return config.make_wsgi_app()
