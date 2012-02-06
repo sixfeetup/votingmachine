17e9bb3b854bd96aea607bb4462cc0734fee2f37
Clayton Parker <clayton@sixfeetup.com>
2012-02-06 10:22:23 -0500

    catch if a user object is None

--- votingmachine/security.py
+++ votingmachine/security.py
@@ -2,4 +2,6 @@ def groupfinder(userid, request):
     root = request.root
     user_folder = root['users']
     user = user_folder.get(userid)
+    if user is None:
+        return []
     return user['groups']
