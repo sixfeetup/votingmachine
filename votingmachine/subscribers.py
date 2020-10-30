8c48cafa9c290931a3c7c0eac07f4e0be0c1b8db
Chrissy Wainwright <chrissy@sixfeetup.com>
2020-10-30 11:08:22 -0500

    move user menu to upper right
    display username with logout link fixes #22

--- votingmachine/subscribers.py
+++ votingmachine/subscribers.py
@@ -1,6 +1,9 @@
 from pyramid.renderers import get_renderer
+from pyramid.security import authenticated_userid
 
 
 def add_base_template(event):
     base = get_renderer('templates/base.pt').implementation()
-    event.update({'base': base})
+    event.update({
+        'base': base,
+        'username': authenticated_userid(event['request'])})
